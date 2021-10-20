6e8196fc9d5b02e06c6c361dbaeb6215a472a606
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2021-10-20 16:53:36 +0700

    export all files

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./dist/base');
+const fs = require('fs');
+const files = fs.readdirSync(`${__dirname}/dist`);
+
+let modules = {};
+files.forEach((file) => {
+  modules = { ...modules, ...require(`./dist/${file}`) };
+});
+
+module.exports = modules;
